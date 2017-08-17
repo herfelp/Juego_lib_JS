4e3311f917c9287985461d9b031b4b71fe8ae5fd
Hernán Felipe Castro <hernan.castro.j@gmail.com>
2017-08-17 10:04:20 -0300

    genera ID dinámicos a las imágenes PNG

--- js/app.js
+++ js/app.js
@@ -16,32 +16,70 @@ $(".main-titulo").css("color", "white");
 //arreglo de columnas
 var cols = ["col-1","col-2","col-3","col-4","col-5","col-6","col-7"];
 
+//hacer el elemento dragabble
+
+var draggable_elem = function(e,dat,num){
+
+    e.draggable({  revert: "invalid", containment: "#panel_"+dat+num , scroll: false });
+
+};
+
+
+
+
 //llenar la pantalla con dulces aleatorios
 
 var llenarpantalla = function(){
-
-  for(i=1;i<8;i++){
-
-       $.each(cols,function(key){
-
-        var num_aleat = 1 + Math.floor(Math.random() * 4);
-
-        var num = key+1;
-        var dat = 8-i
-
-        $( "<img id='elem_"+dat+num+"' src='./image/"+num_aleat+".png' class='elemento ' </img>" ).prependTo(".col-"+num);
-
-       });
-   };
+     for(i=1;i<8;i++){
+         $.each(cols,function(key){
+               var num = key+1;
+               var dat = 8-i
+                $( "<div id='div_"+dat+num+"' class ='elemento_div' ></div>" ).prependTo(".col-"+num);
+         });
+     };
 };
 
  llenarpantalla();
 
 
+
+//asignar ID a cada imagen de cada div y establecer el panel de navegacion y hacerlo draggable
+var asignaid = function(){
+
+     $(".elemento_div").each(function(key){
+       console.log(this.id[4]);
+       console.log(this.id[5]);
+
+        var dat = this.id[4];
+        var num = this.id[5];
+
+       var num_aleat = 1 + Math.floor(Math.random() * 4);
+
+       $( "<img id='elem_"+dat+num+"' src='./image/"+num_aleat+".png' class='elemento ' ></img>" ).prependTo("#div_"+dat+num);
+
+       var left_c =  $("#elem_"+dat+num).position().left - 99;
+       var top_c =  $("#elem_"+dat+num).position().top - 96;
+
+    //incorpora el panel de navegacion para cada elemento draggable
+
+       $( "<div id='panel_"+dat+num+"' class='panel' ></div>" ).prependTo(".panel-tablero");
+
+       $("#panel_"+dat+num ).css({top: top_c, left: left_c, width: 300, height: 288, position:'absolute'});
+
+       draggable_elem($("#elem_"+dat+num+""),dat,num);
+
+    });
+
+};
+
+asignaid();
+
+
  //a cada elemento debe asignarsele una clase si debe o no desaparecer
 
   //establecer cuantos espacios puede ver arriba abajo izquierda derecha
-$(".elemento").each(function(key){
+$(".elemento_div img").each(function(key){
+
 
    var coordi = this.id[5];
    var coordj = this.id[6];
@@ -54,17 +92,18 @@ $(".elemento").each(function(key){
    var izquierda = 6 - derecha;
 
    console.log(coordi+" "+coordj)
-
+   console.log(imagen_elem)
 
 
           for (var i = arriba+2; i < 8; i++) {
             console.log("abajo")
             console.log($("#elem_"+i+""+coordj))
           };
-          
+
           for (var i = abajo+2 ; i < 8; i++) {
             console.log("arriba")
             var aux = 8-i;
+
             console.log($("#elem_"+aux+""+coordj))
           };
 
@@ -78,9 +117,6 @@ $(".elemento").each(function(key){
             var aux = 8-j;
             console.log($("#elem_"+coordi+""+aux))
           };
-
-
-
 
 });
 
